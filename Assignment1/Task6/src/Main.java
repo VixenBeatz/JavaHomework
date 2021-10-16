cceb011952f1f5d34f5980ca7d016b18acae8022
Vixen Beatz <beatzvixen@gmail.com>
2021-10-16 11:53:28 +0300

    A1T6 - 1 Лабораторная Задание 6
    Сделал шестое задание для первой лабораторной

---
+++ Assignment1/Task6/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        SovietComedy sovietComedy = new SovietComedy("12 chairs", 500000, "USSR", "Leonid Gaidai", 1770);
+        sovietComedy.watch();
+        sovietComedy.showInfo();
+    }
+}
